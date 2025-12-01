3c845413bb1d5bbc862af17eafc528f279bca814
Thewisking <wissamdiab122@gmail.com>
2025-12-01 17:35:22 +1030

    set up cmake for running the project

---
+++ small_projects/nonogram_solver/build/CMakeFiles/nonogram_solver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nonogram_solver.dir/src/main.cpp.obj"
+  "CMakeFiles/nonogram_solver.dir/src/main.cpp.obj.d"
+  "libnonogram_solver.dll.a"
+  "nonogram_solver.exe"
+  "nonogram_solver.exe.manifest"
+  "nonogram_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nonogram_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
